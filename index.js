ab95252fb7776cc37182908fbff97d16a91c18a8
unknown <apossebonn@gmail.com>
2024-04-10 00:52:17 -0300

    atualizando_suber_10.04

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+// Alterando placeholder na tela do celular 
+
 const input_cidade = document.getElementById('input_cidade');
+const input_retirada = document.getElementById('input_retirada');
+const input_devolucao = document.getElementById('input_devolucao');
 
+// Celular
 
-input_cidade.placeholder = 'Cidade';
+const mediaQuery = window.matchMedia('(min-width: 481px) and (max-width: 820px)');
+
+function responsividadeCeluar(mediaQuery) {
+    if (!mediaQuery.matches) {
+        input_cidade.placeholder = 'Cidade';
+        input_retirada.placeholder = 'Data de retirada';
+        input_devolucao.placeholder = 'Data de devolução';
+    }
+};
+
+window.addEventListener('resize', function() {
+    responsividadeCeluar(mediaQuery);
+});
+
